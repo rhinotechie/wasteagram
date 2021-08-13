ec828719c94f1ba6c01df0d0d2ec5d58a2851989
Ryan Ong <ryan.ong.jobs@gmail.com>
2021-08-13 00:41:37 -0700

    image picker loads image into app.

--- wasteagram/lib/screens/new_screen.dart
+++ wasteagram/lib/screens/new_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class NewScreen extends StatefulWidget {
   const NewScreen({Key? key}) : super(key: key);
@@ -10,62 +12,80 @@ class NewScreen extends StatefulWidget {
 
 class _NewScreenState extends State<NewScreen> {
   final _formKey = GlobalKey<FormState>();
+  File? photo;
+  final photoPicker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text('Wasteagram'),
-        centerTitle: true,
-      ),
-      body: Container(
-          padding: EdgeInsets.all(10),
-          width: double.infinity,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  child: Placeholder(),
-                  aspectRatio: 3 / 2,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: 'Number of Wasted Items',
-                      border: UnderlineInputBorder()),
-                  onSaved: (value) {
-                    print('Saved!');
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Item amount can\'t be empty.';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-                Spacer(),
-                Container(
-                  width: double.infinity,
-                  height: 100,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.of(context).pop();
-                        }
-                      },
-                      child: Icon(
-                        Icons.cloud_upload,
-                        color: Colors.white,
-                        size: 40,
-                      )),
-                )
-              ],
-            ),
-          )),
-    );
+    if (photo == null) {
+      getPhoto();
+      return Container(
+          color: Colors.black,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ));
+    } else {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('Wasteagram'),
+          centerTitle: true,
+        ),
+        body: Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      width: double.infinity,
+                      height: 200,
+                      child: Image.file(photo!, fit: BoxFit.contain)),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'Number of Wasted Items',
+                        border: UnderlineInputBorder()),
+                    onSaved: (value) {
+                      print('Saved!');
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Item amount can\'t be empty.';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  Spacer(),
+                  Container(
+                    width: double.infinity,
+                    height: 100,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            Navigator.of(context).pop();
+                          }
+                        },
+                        child: Icon(
+                          Icons.cloud_upload,
+                          color: Colors.white,
+                          size: 40,
+                        )),
+                  )
+                ],
+              ),
+            )),
+      );
+    }
+  }
+
+  Future getPhoto() async {
+    final foundPhoto = await photoPicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      photo = File(foundPhoto!.path);
+    });
   }
 }
